7eff9310f9dcd2791eef0ff381d1301bd211a15b
敏 <nimin466@pingan.com.cn>
2017-08-08 18:09:05 +0800

    轮播图自动轮播功能齐备。。

--- plugin/common/_component/carousel/carousel.vue
+++ plugin/common/_component/carousel/carousel.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="carousel-wrapper">
         <div class="carousel-body"
-             :class="{loop:isLoop}"
              ref="_body"
              @drag="_drag"
              @dragend="_dragEnd">
@@ -23,23 +22,26 @@
     import '../../less/carousel.less'
     export default {
         props  : {
-            items : [],
-            click : {
-                default: () => {}
+            items : {
+                default: []
             },
+            click : '',
             isLoop: {
                 default: false
+            },
+            auto  : {
+                default: 0
             }
         },
         data(){
-            this._start = false, this._init = false;
-            this._locked = true;
+            this._timeout = this._init = false;
             return {
                 imgList   : [],
                 indicators: [],
                 itemWidth : 0,
                 maxX      : 0,
-                scrollX   : 0
+                scrollX   : 0,
+                vStatus   : 0,  //当前状态。。0代表锁定状态。1代表滑动状态。2代表自由状态
             }
         },
         created(){
@@ -51,18 +53,31 @@
         updated(){
             this._init || this.initialize(true);
         },
+        beforeDestroy(){
+            clearTimeout(this._timeout);
+        },
         watch  : {
             items(){
-                this._init = !(this._locked = true);
+                this._init = !(this.vStatus = 0);
             },
             isLoop(){
-                this._init = !(this._locked = true);
+                this._init = !(this.vStatus = 0);
+            },
+            auto(){
+                this.vStatus = 0, setTimeout(() => this.vStatus = 2, 16.7);
+            },
+            vStatus(val){
+                clearTimeout(this._timeout);
+                this._timeout = (val === 2 && this.auto) ? setTimeout(() => {
+                    this.vStatus = 1;
+                    this.scrollTo(this.scrollX - this.itemWidth, 350);
+                }, ~~this.auto) : null;
             }
         },
         methods: {
             indicatorClass(idx){
                 let index = Math.abs(this.scrollX / this.itemWidth);
-                return this.isLoop ? (idx + 1) == index : idx == index
+                return this.isLoop ? (idx + 1) == index : idx == index;
             },
             initialize(isMounted){
                 let items       = Object.assign([], this.items);
@@ -73,15 +88,16 @@
                         children   = _body.children || [];
                     this.itemWidth = children[0].offsetWidth;
                     this.maxX      = (1 - children.length) * this.itemWidth;
-                    this.scrollX   = this.isLoop ? -this.itemWidth : 0;
-                    this._init     = !(this._locked = false);
+                    this.scrollX   = this.scrollX || (this.isLoop ? -this.itemWidth : 0);
+                    this._init     = !!(this.vStatus = 2);
+                    this.scrollTo(this.scrollX, 0);
                 }
             },
             _click(item){
                 this.click && this.click(Object.assign({}, item));
             },
             _drag(event){
-                if (this._locked) return;
+                if (this.vStatus === 0) return;
                 var detail    = event.detail || {},
                     direction = detail.direction,
                     deltaX    = detail.deltaX || 0,
@@ -91,31 +107,35 @@
                             deltaX / 4 : deltaX : scrollX <= this.maxX ?
                             direction == 'left' ? deltaX / 4 : deltaX : deltaX),
                         0);
-                    this._start = true;
+                    this.vStatus = 1;
                 }
-                this._start && event.stopPropagation();
+                this.vStatus === 1 && event.stopPropagation();
             },
             _dragEnd(event){
-                if (this._locked) return;
+                if (this.vStatus === 0) return;
                 var detail    = event.detail || {},
                     direction = detail.direction,
                     deltaX    = Math.abs(detail.deltaX),
                     speed     = deltaX / detail.deltaTime,
-                    itemWidth = this.itemWidth,
-                    maxX      = this.maxX,
-                    scrollX   = this.scrollX;
-                deltaX        = (speed > .35 && deltaX * 4 > itemWidth) || deltaX * 2 > itemWidth ? direction == 'left' ? -itemWidth : itemWidth : 0;
-                scrollX += deltaX, this._locked = true, this._start = false;
-                this.setTranslate(scrollX = scrollX > 0 ? 0 : scrollX < maxX ? maxX : scrollX, 350);
-                setTimeout(() => {
-                    this.isLoop && this.setTranslate(scrollX = scrollX >= 0 ? maxX + itemWidth : scrollX <= maxX ? -itemWidth : scrollX, 0)
-                    this._locked = false, this.scrollX = scrollX;
-                }, 350);
+                    itemWidth = this.itemWidth;
+                deltaX        = (speed > .35 && deltaX * 4 > itemWidth) || deltaX * 2 > itemWidth ?
+                    direction == 'left' ? -itemWidth : itemWidth : 0;
+                this.vStatus = 0, this.scrollTo(this.scrollX + deltaX, 350);
+            },
+            scrollTo(scrollX, time){
+                let itemWidth = this.itemWidth,
+                    maxX      = this.maxX;
+                this.setTranslate(scrollX = scrollX > 0 ? 0 : scrollX < maxX ? maxX : scrollX, time);
+                time && setTimeout(() => {
+                    this.isLoop && this.setTranslate(scrollX = scrollX >= 0 ? maxX + itemWidth :
+                        scrollX <= maxX ? -itemWidth : scrollX, 0);
+                    this.vStatus = 2, this.scrollX = scrollX;
+                }, time)
             },
             setTranslate(x, time){
                 let _body                            = this.$refs._body;
                 _body.style.webkitTransitionDuration = (time || 0) + 'ms';
-                _body.style.webkitTransform          = 'translateX(' + x + 'px)';
+                _body.style.webkitTransform          = 'translate3d(' + x + 'px,0,0) translateZ(0)';
             }
         }
     }
--- src/page/carousel.vue
+++ src/page/carousel.vue
@@ -1,6 +1,13 @@
 <template>
     <v-page :options="options">
+        <h3>循环轮播</h3>
         <v-carousel :items="items" :click="click" is-loop="true"/>
+        <h3>自动循环轮播</h3>
+        <v-carousel :items="items" :click="click" is-loop="true" :auto="auto"/>
+        <h3>非循环轮播</h3>
+        <v-carousel :items="items" :click="click"/>
+        <h3>非循环轮播</h3>
+        <v-carousel :items="items" :click="click" :auto="auto"/>
     </v-page>
 </template>
 <script type="text/babel">
@@ -16,15 +23,18 @@
             return {
                 options: {
                     title: {
-                        value: '折叠面板',
+                        value: '轮播图',
                     },
                 },
                 items  : [
                     {src: 'http://p1.meituan.net/460.280/deal/bb1f679bbd624aea5f20dfe90875c1c643869.jpg@460w_280h_1e_1c'},
                     {src: 'http://p1.meituan.net/deal/f8ada90c06fdbfbed00a1de6adadc2ad30696.jpg@460w_280h_1e_1c'},
                     {src: 'http://p1.meituan.net/deal/7e2608b10ad9805ad48e6927aec1e68446050.jpg@460w_280h_1e_1c'},
-                ]
+                ],
+                auto   : 8000
             }
+        },
+        mounted(){
         },
         methods   : {
             click(){
